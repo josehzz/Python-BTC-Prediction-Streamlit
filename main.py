1917a94e9826b648accc2bbeebc6c591684a6bf2
josehzz <63607627+josehzz@users.noreply.github.com>
2025-06-17 19:04:45 -0400

    Added env token

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ st.plotly_chart(fig, use_container_width=True)
 # Links
 medium_url = ""
 linkedin_url = "https://www.linkedin.com/in/josehzz/"
-st.write("How it was made: (Comming soon)")
-st.write("Contact me: [link](%s)" % linkedin_url)
+st.write("How it was made: (Coming soon)")
+st.write("Contact me: [linkedin](%s)" % linkedin_url)
 
 st.write("Last Update: ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
